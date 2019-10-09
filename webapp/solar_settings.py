498ed5604af1448f65e5d88d84797fd887ca940d
szbeni <szbeni@gmail.com>
2019-10-09 21:41:18 +0100

    websocket testing

--- webapp/solar_settings.py
+++ webapp/solar_settings.py
@@ -1,10 +1,10 @@
 class SolarSettings:
     fps =  100.0
-    serial_pub_address = "tcp://127.0.0.1:5555"
+    serial_pub_address = "ws://127.0.0.1:5555"
     serial_msg_address = "tcp://127.0.0.1:5556"
     serial_settings = {
-        'address_pub': 'tcp://0.0.0.0:5555',
-        'address_msg': 'tcp://0.0.0.0:5556',
+        'address_pub': 'ws://127.0.0.1:5555',
+        'address_msg': 'tcp://127.0.0.1:5556',
         'port': '/dev/rfcomm0',
         'baud': 9600
     }
